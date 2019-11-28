45be2cfa2ba07e8b94b49fcb26149f069713379a
zxd3333 <1274752060@qq.com>
2019-11-28 21:12:11 +0800

    MyBatis简单的增删改查

--- src/main/java/ssm/mybatis1/dao/StudentMapper.java
+++ src/main/java/ssm/mybatis1/dao/StudentMapper.java
@@ -5,5 +5,14 @@ import ssm.mybatis1.pojo.Student;
 import java.util.List;
 
 public interface StudentMapper {
+    //获取所有学生
     List<Student> getAll();
+    //查找一个学生
+    Student selectById(int sid);
+    //添加一个学生
+    void addStudent(Student student);
+    //更新一个学生
+    void updateStudent(Student student);
+    //删除一个学生
+    void deleteStudentById(int sid);
 }
--- src/test/java/ssm/mybatis1/dao/StudentMapperTest.java
+++ src/test/java/ssm/mybatis1/dao/StudentMapperTest.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class StudentMapperTest {
 
     @Test
+    //获取所有学生
     public void test(){
         //获得SqlSession对象
         SqlSession sqlSession = MyBatisUtil.getSqlSession();
@@ -19,6 +20,53 @@ public class StudentMapperTest {
         for (Student s : allStudent){
             System.out.println(s);
         }
+        sqlSession.close();
+    }
+
+    @Test
+    //根据学号获取学生
+    public void test1(){
+        SqlSession sqlSession = MyBatisUtil.getSqlSession();
+        StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
+        Student student = mapper.selectById(1);
+        System.out.println(student);
+        sqlSession.close();
+    }
+
+    @Test
+    //添加一个学生
+    public void test2(){
+        SqlSession sqlSession = MyBatisUtil.getSqlSession();
+        StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
+        Student student = new Student(10,"老人",100);
+        mapper.addStudent(student);
+        System.out.println(student);
+        //对数据库有更新时必须提交事务
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    //根据学号更新一个学生
+    public void test3(){
+        SqlSession sqlSession = MyBatisUtil.getSqlSession();
+        StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
+        Student student = new Student(10,"错误的人",666);
+        mapper.updateStudent(student);
+        System.out.println(student);
+        //对数据库有更新时必须提交事务
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+    @Test
+    //根据学号删除一个学生
+    public void test4(){
+        SqlSession sqlSession = MyBatisUtil.getSqlSession();
+        StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
+        mapper.deleteStudentById(10);
+        //对数据库有更新时必须提交事务
+        sqlSession.commit();
         sqlSession.close();
     }
 }
