d9f0601d5fd500c339b358b04f4ea393bfb23b25
zxd3333 <1274752060@qq.com>
2019-12-06 16:39:20 +0800

    静态代理(修改一处错误)

--- Spring-3/src/main/java/ssm/spring/Customer.java
+++ Spring-3/src/main/java/ssm/spring/Customer.java
@@ -10,7 +10,8 @@ public class Customer {
         //首先需要一个房东，他要出租自己的房子
         Host h = new Host();
         //这里中介帮他发布信息
-        Agency agency = new Agency(h);
+        Agency agency = new Agency();
+        agency.setHost(h);
         //然后我们找到中介，再调用中介的租房方法，在Agency类中可以看出，中介的租房方法本质上还是房东租房
         //但是中介的租房方法还有自己的方法，也就是中间商赚差价的意思，也可以理解成代理费。
         agency.rent();
